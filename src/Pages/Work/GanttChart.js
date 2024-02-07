34c3f5f4d4265dce075340d3361723b2830df910
tushar.deshmukh <tushar.deshmukh@laminaar.in>
2024-02-07 20:29:19 +0530

    work

--- src/Pages/Work/GanttChart.js
+++ src/Pages/Work/GanttChart.js
@@ -77,11 +77,138 @@ function GanttChart() {
               height: "160px",
               width: "100%",
               background:
-                "linear-gradient(180deg,rgba(8,8,10,0) 10%,var(--token-55071dc9-92d0-4cdb-8ef9-67f5658ea748, #000000) 85%)",
+                "linear-gradient(180deg,rgba(8,8,10,0) 10%,var(--token-55071dc9-92d0-4cdb-8ef9-67f5658ea748, #000000) 69%)",
             }}
           ></div>
         </div>
       </div>
+      <div
+        style={{
+          height: "auto",
+          width: "100%",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          fontSize: "100px",
+          color: "white",
+          paddingBottom: "160px",
+          fontWeight: "100",
+          backdropFilter: "contrast(20)",
+        }}
+      >
+        Project overview
+      </div>
+      <div
+        style={{
+          height: "auto",
+          width: "100%",
+          fontSize: "25px",
+          color: "white",
+          paddingBottom: "160px",
+          fontWeight: "100",
+          backdropFilter: "contrast(20)",
+          padding: "0px 60px 80px 60px",
+          boxSizing: "border-box",
+          lineHeight: "40px",
+          textAlign: "center",
+        }}
+      >
+        Introducing a cutting-edge{" "}
+        <span style={{ color: "#6366f1", fontWeight: "normal" }}>
+          Gantt Chart{" "}
+        </span>
+        application meticulously crafted from the ground up using{" "}
+        <span style={{ color: "#6366f1", fontWeight: "normal" }}>React</span>.
+        This innovative tool boasts advanced features tailored for seamless
+        scalability and lightning-fast performance, ensuring efficient project
+        management. With a fully customizable interface driven by props, users
+        have unparalleled control over their Gantt charts. What sets this
+        solution apart is its reliance solely on internal functionalities,
+        eschewing external packages for a lean and optimized experience.
+        Leveraging SignalR for real-time updates, users can collaborate
+        effortlessly and stay synchronized in dynamic environments.
+        <div
+          style={{
+            height: "auto",
+            display: "flex",
+            width: "100%",
+            gap: "20px",
+            marginTop: "20px",
+          }}
+        >
+          <div
+            style={{
+              fontSize: "16px",
+              padding: "5px 20px",
+              fontWeight: "bold",
+              background: "grey",
+              borderRadius: "24px",
+              backgroundColor: "rgb(37, 37, 37)",
+            }}
+          >
+            <span style={{ filter: "contrast(0.5)" }}>
+              Air Traffic Control System{" "}
+            </span>
+          </div>
+          <div
+            style={{
+              fontSize: "16px",
+              padding: "5px 20px",
+              fontWeight: "bold",
+              background: "grey",
+              borderRadius: "24px",
+              backgroundColor: "rgb(37, 37, 37)",
+            }}
+          >
+            <span style={{ filter: "contrast(0.5)" }}>
+              Airport Ground Handeling
+            </span>
+          </div>
+          <div
+            style={{
+              fontSize: "16px",
+              padding: "5px 20px",
+              fontWeight: "bold",
+              background: "grey",
+              borderRadius: "24px",
+              backgroundColor: "rgb(37, 37, 37)",
+            }}
+          >
+            <span style={{ filter: "contrast(0.5)" }}>
+              Airport Crew Duties Management
+            </span>
+          </div>
+          <div
+            style={{
+              fontSize: "16px",
+              padding: "5px 20px",
+              fontWeight: "bold",
+              background: "grey",
+              borderRadius: "24px",
+              backgroundColor: "rgb(37, 37, 37)",
+            }}
+          >
+            <span style={{ filter: "contrast(0.5)" }}>
+              Crew Training Management
+            </span>
+          </div>
+          <div
+            style={{
+              fontSize: "16px",
+              padding: "5px 20px",
+              fontWeight: "bold",
+              background: "grey",
+              borderRadius: "24px",
+              backgroundColor: "rgb(37, 37, 37)",
+            }}
+          >
+            <span style={{ filter: "contrast(0.5)" }}>
+              Flight Scheduling & Tracking
+            </span>
+          </div>
+        </div>
+      </div>
+
       <div
         style={{
           width: "100%",
@@ -100,12 +227,25 @@ function GanttChart() {
             justifyContent: "center",
             alignItems: "center",
             boxSizing: "border-box",
-            border: "5px solid white",
-            borderRadius: "5px",
+            position: "relative",
+            padding: "6px",
+            background: "rgb(37 37 37)",
+            borderRadius: "24px",
           }}
         >
+          <div
+            style={{
+              position: "absolute",
+              bottom: "0",
+              left: "0",
+              height: "50px",
+              width: "101%",
+              background:
+                "linear-gradient(180deg,rgba(8,8,10,0) 10%,var(--token-55071dc9-92d0-4cdb-8ef9-67f5658ea748, #000000) 69%)",
+            }}
+          ></div>
           <video
-            style={{ objectFit: "cover" }}
+            style={{ objectFit: "cover", borderRadius: "24px" }}
             height="100%"
             width="100%"
             src={dnd}
@@ -119,13 +259,35 @@ function GanttChart() {
             height: "352px",
             width: "50%",
             display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            padding: "10px",
+            // justifyContent: "center",
+            // alignItems: "center",
+            // padding: "10px",
             boxSizing: "border-box",
-            border: "1px solid white",
+            flexDirection: "column",
+            color: "white",
+            fontSize: "30px",
+            // fontWeight:"100"
           }}
-        ></div>
+        >
+          <div style={{ color: "#6366f1" }}>Dragging</div>
+          <div style={{ fontSize: "24px" }}>
+            <ul>
+              <li>
+                Seamless drag-and-drop functionality allows users to
+                effortlessly reorganize events within the Gantt chart.
+              </li>
+              <li>
+                Intuitive event creation/deletion feature enables users to
+                remove unwanted events with ease.
+              </li>
+              <li>
+                Backend integration ensures that data manipulations, such as
+                dragging, dropping, or deleting events, are reflected accurately
+                and efficiently.
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
 
       <div
@@ -147,12 +309,25 @@ function GanttChart() {
             justifyContent: "center",
             alignItems: "center",
             boxSizing: "border-box",
-            border: "5px solid white",
-            borderRadius: "5px",
+            position: "relative",
+            padding: "6px",
+            background: "rgb(37 37 37)",
+            borderRadius: "24px",
           }}
         >
+          <div
+            style={{
+              position: "absolute",
+              bottom: "0",
+              left: "0",
+              height: "50px",
+              width: "101%",
+              background:
+                "linear-gradient(180deg,rgba(8,8,10,0) 10%,var(--token-55071dc9-92d0-4cdb-8ef9-67f5658ea748, #000000) 69%)",
+            }}
+          ></div>
           <video
-            style={{ objectFit: "cover" }}
+            style={{ objectFit: "cover", borderRadius: "24px" }}
             height="100%"
             width="100%"
             src={elongate}
@@ -166,13 +341,39 @@ function GanttChart() {
             height: "352px",
             width: "50%",
             display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            padding: "10px",
+            // justifyContent: "center",
+            // alignItems: "center",
+            // padding: "10px",
             boxSizing: "border-box",
-            border: "1px solid white",
+            flexDirection: "column",
+            color: "white",
+            fontSize: "30px",
+            fontWeight: "100",
           }}
-        ></div>
+        >
+          <div style={{ color: "#6366f1" }}>Extending</div>
+          <div style={{ fontSize: "24px" }}>
+            <ul>
+              <li>
+                Flexible event extension capability empowers users to
+                dynamically adjust the duration of events within the Gantt
+                chart.
+              </li>
+              <li>
+                User can manipulate the duration by referring the timeline
+                (IST/UTC) ruler in the header.
+              </li>
+              <li>
+                Upon clicking an event bar, dynamic guidelines appear,
+                facilitating alignment and comparison with the timeline ruler
+                positioned above.
+              </li>
+              <li>
+                The <span style={{color:"rgb(99, 102, 241)"}}>cell render</span> feature allows the bars to take any shape & size as per user requirement.
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
 
       <div
@@ -193,11 +394,25 @@ function GanttChart() {
             justifyContent: "center",
             alignItems: "center",
             boxSizing: "border-box",
-            borderRadius: "5px",
+            position: "relative",
+            padding: "6px",
+            background: "rgb(37 37 37)",
+            borderRadius: "24px",
           }}
         >
+          <div
+            style={{
+              position: "absolute",
+              bottom: "0",
+              left: "0",
+              height: "50px",
+              width: "101%",
+              background:
+                "linear-gradient(180deg,rgba(8,8,10,0) 10%,var(--token-55071dc9-92d0-4cdb-8ef9-67f5658ea748, #000000) 69%)",
+            }}
+          ></div>
           <video
-            style={{ objectFit: "cover" }}
+            style={{ objectFit: "cover", borderRadius: "24px" }}
             height="100%"
             width="100%"
             src={movement}
@@ -211,14 +426,50 @@ function GanttChart() {
             height: "352px",
             width: "50%",
             display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            padding: "10px",
+            // justifyContent: "center",
+            // alignItems: "center",
+            // padding: "10px",
             boxSizing: "border-box",
-            border: "1px solid white",
+            flexDirection: "column",
+            color: "white",
+            fontSize: "30px",
+            fontWeight: "100",
           }}
-        ></div>
+        >
+          <div style={{ color: "#6366f1" }}>Moving</div>
+          <div style={{ fontSize: "24px" }}>
+            <ul>
+              <li>
+                Dynamic event bar movement functionality enables users to
+                reposition events within the Gantt chart effortlessly.
+              </li>
+              <li>
+                Users can efficiently adjust event placement to reflect changes
+                in project scheduling or priorities.
+              </li>
+              <li>
+                As users zoom in on the Gantt chart, event bars expand
+                proportionally to maintain clarity and visibility within the
+                increased view.
+              </li>
+              <li>Many more innovative features in the package..</li>
+            </ul>
+          </div>
+        </div>
       </div>
+      <div style={{color:"white", width:"100%", height:"auto", textAlign:"center", fontSize:"24px", padding:"60px 120px", boxSizing:"border-box"}}>
+          The Gantt chart application is powered by an innovative 
+          <span style={{ color: "#6366f1", fontWeight: "normal" }}>
+          {" "}DOM Virtualization{" "}
+        </span> feature, enabling unparalleled scalability without
+          sacrificing performance. In an impressive testament to its robustness,
+          our application has undergone rigorous testing with datasets
+          containing up to 
+          <span style={{ color: "#6366f1", fontWeight: "normal" }}>
+          {" "}10 million records{" "}
+        </span>, surpassing expectations by
+          maintaining optimal performance without any throttle.
+        </div>
     </div>
   );
 }
--- src/Pages/Work/Work.js
+++ src/Pages/Work/Work.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import ganttmain from "../../assets/gantttow.png";
 
 function Work() {
   return (
@@ -28,6 +29,7 @@ function Work() {
         <div style={{ fontSize: "120px" }}>Let me show</div>
         <div style={{ fontSize: "120px" }}>you</div>
       </div>
+
       <div
         style={{
           display: "flex",
@@ -42,22 +44,58 @@ function Work() {
           boxSizing: "border-box",
         }}
       >
-        <div class="card-work">
-          <Link to="/work/ganttchart" onClick={() => window.scrollTo(0, 0)}>
-            Gantt
-          </Link>
+        <div class="cardx" style={{ height: "350px", width: "500px" }}>
+          <div class="contentx" style={{ height: "100%", width: "100%",padding:"3px",boxSizing:"border-box" }}>
+            <img
+              src={ganttmain}
+              style={{ borderRadius: "24px" }}
+              alt=""
+              height={"100%"}
+              width={"100%"}
+            />
+            <div class="frontx">
+              <h3 class="titlex" style={{height:"15px", width:"50px", background:"#12372A", position:"absolute", top:"32px", left:"200px", borderRadius:"24px",boxShadow:"12px 20px 15px -3px black"}}></h3>
+              <h3 class="titlex" style={{height:"15px", width:"80px", background:"#436850", position:"absolute", top:"64px", left:"100px", borderRadius:"24px",boxShadow:"12px 20px 15px -3px black"}}></h3>
+              <h3 class="titlex" style={{height:"15px", width:"50px", background:"#747264", position:"absolute", top:"99px", left:"320px", borderRadius:"24px",boxShadow:"12px 20px 15px -3px black"}}></h3>
+              <h3 class="titlex" style={{height:"15px", width:"90px", background:"#ADBC9F", position:"absolute", top:"133px", left:"160px", borderRadius:"24px",boxShadow:"12px 20px 15px -3px black"}}></h3>
+              <h3 class="titlex" style={{height:"15px", width:"105px", background:"#436850", position:"absolute", top:"169px", left:"210px", borderRadius:"24px",boxShadow:"12px 20px 15px -3px black"}}></h3>
+              <h3 class="titlex" style={{height:"15px", width:"83px", background:"#607274", position:"absolute", top:"232px", left:"356px", borderRadius:"24px",boxShadow:"12px 20px 15px -3px black"}}></h3>
+
+              <h3 class="titlex" style={{height:"15px", width:"70px", background:"#3C3633", position:"absolute", top:"200px", left:"110px", borderRadius:"24px",boxShadow:"12px 20px 15px -3px black"}}></h3>
+              <h3 class="titlex" style={{height:"15px", width:"70px", background:"#436850", position:"absolute", top:"304px", left:"300px", borderRadius:"24px",boxShadow:"12px 20px 15px -3px black"}}></h3>
+              <h3 class="titlex" style={{height:"15px", width:"70px", background:"#12372A", position:"absolute", top:"269px", left:"210px", borderRadius:"24px",boxShadow:"12px 20px 15px -3px black"}}></h3>
+
+
+
+            </div>
+
+            <div class="backx">
+              <div style={{fontSize:"60px", color:"white"}}>Gantt Chart</div>
+              
+            </div>
+          </div>
         </div>
-        <div class="card-work">
-          <Link to="/work/calendar" onClick={() => window.scrollTo(0, 0)}>
-            Calendar
-          </Link>
-        </div>
-        <div class="card-work">
-          <Link to="/work/portal" onClick={() => window.scrollTo(0, 0)}>
-            Portal
-          </Link>
-        </div>
-        <div class="card-work"></div>
+
+        {/* <div class="cardx" style={{ height: "350px", width: "500px" }}>
+          <div class="contentx" style={{ height: "100%", width: "100%" }}>
+            <img
+              src={ganttmain}
+              style={{ borderRadius: "24px" }}
+              alt=""
+              height={"100%"}
+              width={"100%"}
+            />
+            <div class="frontx">
+              <h3 class="titlex">Gantt Chart</h3>
+            </div>
+
+            <div class="backx">
+              <p class="descriptionx">
+                Cool description so you can read it too my friend
+              </p>
+            </div>
+          </div>
+        </div> */}
       </div>
     </div>
   );
